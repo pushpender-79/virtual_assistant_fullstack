fe646a54199fb8ded6eca63ad717a461b45709dc
pushpender-79 <pushpenderchauhan79@gmail.com>
2026-02-28 17:20:03 +0530

    feat:setup React frontend with signin and signup pages

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,13 @@ import express from "express"
 import connectDb from "./config/db.js"
 import authrRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app = express()
+app.use(cors({
+origin:"http://localhost:5173",
+credentials:true
+}))
 const port = process.env.PORT || 5000
 
 app.use(express.json())
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,101 @@
+import React, { useContext } from "react";
+import bg from "../assets/authBG.png";
+import { IoIosEye } from "react-icons/io";
+import { useState } from "react";
+import { IoIosEyeOff } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+import { userDataContext } from "../context/userContext";
+import axios from "axios";
+
+function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const { serverUrl } = useContext(userDataContext);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState("");
+  const [err, setErr] = useState("");
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    setErr("");
+    setLoading(true);
+    try {
+      let result = await axios.post(
+        `${serverUrl}/api/auth/signin`,
+        { email, password },
+        { withCredentials: true },
+      );
+      console.log(result.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setErr(error.response.data.message);
+    }
+  };
+  return (
+    <div
+      className=" w-full h-[100vh] bg-cover flex justify-center items-center"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <form
+        className="w-[90%] h-[600px] max-w-[500px] bg-[#0000005c] backdrop-blur-sm shadow-lg shadow-black-950 flex flex-col items-center justify-center gap-[20px] px-[20px]"
+        onSubmit={handleSignIn}
+      >
+        <h1 className="text-white text-[30px] font-semibold mb-[30px]">
+          Sign In to <span className="text-blue-400">Virtual Assistant</span>
+        </h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300
+        px-[20px] py-[10px] rounded-full text-[18px]"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <div className="w-full h-[60px] border-2 border-white bg-transparent text-white rounded-full text-[18px] relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="password"
+            className="w-full h-full rounded-full outline-none bg-transparent placeholder-gray-300 px-[20px] py-[10px]"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+
+          {!showPassword && (
+            <IoIosEye
+              className="absolute top-[18px] right-[20px] text-white w-[25px] h-[25px] cursor-pointer"
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+          {showPassword && (
+            <IoIosEyeOff
+              className="absolute top-[18px] right-[20px] text-white w-[25px] h-[25px] cursor-pointer"
+              onClick={() => setShowPassword(false)}
+            />
+          )}
+        </div>
+        {err.length > 0 && <p className="text-red-500 text-[20px]">*{err}</p>}
+        <button
+          className="min-w-[120px] h-[40px] mt-[15px] bg-white text-black font-semibold text-[18px] rounded-full"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+        <p
+          className="text-[white] text-[16px] cursor-pointer"
+          onClick={() => navigate("/signup")}
+        >
+          want to create a new account?{" "}
+          <span className="text-blue-400">Sign Up</span>{" "}
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
